e7014af48d4c32a4eb017365611535dfa8e1be66
Guy Wilson <wilson.guy@gmail.com>
2021-12-23 10:03:14 +0000

    Initial commit

---
+++ src/xcode.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <regex.h>
+
+using namespace std;
+
+#define MODE_XWORD                  1
+#define MODE_ANAGRAM                2
+
+#define DICTIONARY_FILE             "./dictionary.db"
+
+#define ERROR_BUF_SIZE              128
+#define REGEX_MATCH_LEN             64
+
+
+void inline printUsage() {
+    cout << "Usage:" << endl;
+    cout << "    xcode <options> [input string]" << endl << endl;
+    cout << "    -x crossword mode, list all possible matches for input string" << endl;
+    cout << "    -a anagram mode, list all anagrams for the word in input string" << endl << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    int             mode = 0;
+    int             rtn;
+    char *          pszInput;
+    char *          pszDictionary;
+    FILE *          fptr;
+    char            szErrorString[ERROR_BUF_SIZE];
+    char            szMatch[REGEX_MATCH_LEN];
+    uint32_t        dictionaryLen;
+
+    if (argc > 2) {
+        for (int i = 1;i < argc;i++) {
+            if (argv[i][0] == '-') {
+                if (strncmp(&argv[i][1], "x", 2) == 0) {
+                    mode = MODE_XWORD;
+                }
+                else if (strncmp(&argv[i][1], "a", 2) == 0) {
+                    mode = MODE_ANAGRAM;
+                }
+                else {
+                    cout << "Invalid argument: " << &argv[i][0] << endl << endl;
+                    printUsage();
+                    exit(-1);
+                }
+            }
+        }
+    }
+    else {
+        printUsage();
+        exit(-1);
+    }
+
+    pszInput = strdup(&argv[2][0]);
+
+    fptr = fopen(DICTIONARY_FILE, "rt");
+
+    if (fptr == NULL) {
+        cout << "Failed to open dictionary file '" << DICTIONARY_FILE << "'" << endl;
+        exit(-1);
+    }
+
+    fseek(fptr, 0L, SEEK_END);
+    dictionaryLen = ftell(fptr);
+    fseek(fptr, 0L, SEEK_SET);
+
+    pszDictionary = (char *)malloc(dictionaryLen);
+
+    if (pszDictionary == NULL) {
+        cout << "Failed to allocate memory for dictionary" << endl;
+        fclose(fptr);
+        exit(-1);
+    }
+
+    fread(pszDictionary, 1, dictionaryLen, fptr);
+
+    fclose(fptr);
+
+    if (mode == MODE_XWORD) {
+        string regexInitialiser = "^("; 
+
+        for (int j = 0;j < (int)strlen(pszInput);j++) {
+            if (pszInput[j] == '?') {
+                regexInitialiser += "[a-z]{1}";
+            }
+            else if (isalpha(pszInput[j])) {
+                regexInitialiser += pszInput[j];
+            }
+            else {
+                cout << "Invalid character in input string" << endl;
+                free(pszDictionary);
+                exit(-1);
+            }
+        }
+
+        regexInitialiser += ")$";
+
+        cout << "Regex string '" << regexInitialiser << "'" << endl;
+
+        char *          s = pszDictionary;
+        regex_t         regex;
+        regmatch_t      pmatch[1];
+        regoff_t        matchOffset;
+        regoff_t        matchLength;
+
+        rtn = regcomp(&regex, regexInitialiser.c_str(), REG_EXTENDED | REG_NEWLINE);
+
+        if (rtn) {
+            regerror(rtn, &regex, szErrorString, 128);
+
+            cout << "Failed to compile regular expression '" << szRegexInitialiser << "': " << szErrorString;
+            free(pszDictionary);
+            exit(-1);
+        }
+
+        cout << "Matches for '" << pszInput << "':" << endl;
+
+        while (!regexec(&regex, s, 1, pmatch, 0)) {
+            matchOffset = pmatch[0].rm_so;
+            matchLength = pmatch[0].rm_eo - pmatch[0].rm_so;
+
+            if (matchLength > REGEX_MATCH_LEN) {
+                cout << "*** Match longer than buffer, skipping..." << endl;
+            }
+            else {
+                strncpy(szMatch, &s[matchOffset], matchLength);
+
+                cout << szMatch << endl;
+            }
+
+            s += pmatch[0].rm_eo;
+        }
+    }
+    else if (mode == MODE_ANAGRAM) {
+
+    }
+    else {
+        cout << "Invalid mode: " << mode << endl << endl;
+        printUsage();
+        free(pszDictionary);
+        exit(-1);
+    }
+
+    free(pszDictionary);
+
+    return 0;
+}
